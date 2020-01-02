abb1f7192aef030d9a06030cdd656a09bc190f16
Andrei Ivanov <dmkfasi@gmail.com>
2020-01-02 22:20:51 +0300

    Removed special case for 2 item long chain

--- day1.py
+++ day1.py
@@ -2,9 +2,9 @@ import random
 
 class Solution(object):
 	def getRandomDominoes(l):
-		random.seed()
 		dominoes = ''
 		for i in range(l):
+			random.seed()
 			d = random.randint(0, 2)
 			# 0 equals dot, 1 equals right, 2 equals left
 			if d == 0:
@@ -23,32 +23,24 @@ class Solution(object):
 			l = len(dominoes)
 			d = list(dominoes)
 			out = []
-			vn = ''
+			vnext = ''
 
 			if l < 2:
 				return ''.join(dominoes)
 
-			if l == 2:
-				if d[0] == 'R':
-					dominoes = dominoes.replace('.', 'R')
-				if d[1] == 'L':
-					dominoes = dominoes.replace('.', 'L')
-
-				return dominoes
-
 			for i, v in enumerate(d):
 
-				if vn is not '':
-					val = vn
+				if vnext is not '':
+					val = vnext
 					out.append(val)
-					vn = ''
+					vnext = ''
 					continue
 				else:
 					val = v
 					out.append(val)
 
 				if v == 'R':
-					vn = v
+					vnext = v
 
 				if v == 'L':
 					if i > 1 and out[i - 2] == 'R':
@@ -63,7 +55,8 @@ class Solution(object):
 			return ''.join(out)
 
 #str = '..R...L..R.'
-str = Solution.getRandomDominoes(5)
+#str = '.L'
+str = Solution.getRandomDominoes(15)
 print(' Input: ' + str)
 print('Output: ' + Solution().pushDominoes(str))
 
